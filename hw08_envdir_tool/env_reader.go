2a2297baeb9e0da5a91a4658f8c7e0f589199e59
Dolgova Galina <g.dolgova@id-network.ru>
2022-06-14 00:51:52 +0300

    HW08: Implement envdir utility

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,6 +1,15 @@
 package main
 
-type Environment map[string]EnvValue
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+type Environment map[string]*EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
@@ -11,6 +20,72 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envValues := make(Environment)
+
+	if dir == "" {
+		return envValues, nil
+	}
+
+	fileList, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	re := regexp.MustCompile(`[^\w_]`)
+	for _, file := range fileList {
+		if file.IsDir() {
+			continue // skip directory
+		}
+
+		fi, err := file.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		name := fi.Name()
+		if re.Match([]byte(name)) {
+			continue // check incorrect symbols in filename
+		}
+
+		if fi.Size() == 0 {
+			envValues[name] = &EnvValue{Value: "", NeedRemove: true}
+			continue // variable needs to be deleted
+		}
+
+		line, err := firstFileLine(filepath.Join(dir, name)) // parse value from file
+		if err != nil {
+			return nil, err
+		}
+		envValues[name] = &EnvValue{Value: strings.TrimRight(line, "\t "), NeedRemove: false}
+	}
+
+	return envValues, nil
+}
+
+func firstFileLine(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var bline []byte
+	for scanner.Scan() {
+		bline = scanner.Bytes()
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return prepareLine(bline), nil
+}
+
+func prepareLine(bline []byte) string {
+	bline = bytes.ReplaceAll(bline, []byte{0}, []byte{'\n'})
+	line := string(bline)
+	line = strings.TrimRight(line, "\t ")
+	return line
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,59 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var EmptyEnv = make(Environment)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("simple", func(t *testing.T) {
+		code := RunCmd([]string{"echo", "call", "me"}, EmptyEnv)
+		require.Equal(t, 0, code)
+	})
+
+	t.Run("without args", func(t *testing.T) {
+		code := RunCmd([]string{"pwd"}, EmptyEnv)
+		require.Equal(t, 0, code)
+	})
+
+	t.Run("exit code 2", func(t *testing.T) {
+		code := RunCmd([]string{"ls", "qazaqazaq"}, EmptyEnv) // no such file or directory
+		require.Equal(t, 2, code)
+	})
+}
+
+func TestSetEnv(t *testing.T) {
+	t.Run("several sets", func(t *testing.T) {
+		expected := make(map[string]string)
+		expected["homework"] = "07" // unexisted
+		expected["OLDPWD"] = "/usr" // existed
+
+		env := make(Environment)
+		for name := range expected {
+			env[name] = &EnvValue{Value: expected[name], NeedRemove: false}
+		}
+
+		SetEnv(env)
+
+		for name := range expected {
+			require.Equal(t, expected[name], os.Getenv(name))
+		}
+	})
+
+	t.Run("remove", func(t *testing.T) {
+		env := make(Environment)
+		name := "kapuki"
+		env[name] = &EnvValue{Value: "kanuki", NeedRemove: false}
+
+		SetEnv(env)
+		require.Equal(t, env[name].Value, os.Getenv(name)) // written
+
+		env[name].NeedRemove = true
+		SetEnv(env)
+		require.Equal(t, "", os.Getenv(name)) // removed
+	})
 }
